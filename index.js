417624ed030dc19a6c0acdc28571889b97d15158
bibinbabumj <bibinbabumj93@gmail.com>
2023-03-28 04:54:16 +0100

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const PORT=process.env.PORT||3000
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+     res.send("hello")        
+})
+
+
+
+app.listen(PORT,()=>{
+    console.log("listen")
+})
