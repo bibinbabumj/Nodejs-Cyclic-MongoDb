7e8e084ab0139acc851d161e64b2f62809e09518
bibinbabumj <bibinbabumj93@gmail.com>
2023-04-07 17:28:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const Hobbies = require("./models/hobbies");
 const Education = require("./models/education");
 const Project = require("./models/projects");
 const Experience = require("./models/experience");
-const { user } = require("firebase-functions/v1/auth");
 
 
 dotenv.config();
@@ -36,11 +35,12 @@ const connectDb= async()=>{
 app.get("/user",async(req,res)=>{
 try {
     const users = await User.find();
-   // if (users.length > 0) {
-      res.status(200).json(users);
-   // } else {
-    //  res.status(404).send("No users found");
-   // }
+   if (users.length > 0) {
+      res.status(200).json(users[0]);
+      console.log("user")
+    } else {
+  res.status(404).send("No users found");
+}
 } catch (error) {
     console.error(error.message);
     res.status(500).send("Something went wrong");
