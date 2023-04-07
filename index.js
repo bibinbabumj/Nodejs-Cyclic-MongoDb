3c8edc18421c4fcd6dcdf200bdc5b9d9c5539103
bibinbabumj <bibinbabumj93@gmail.com>
2023-04-07 13:09:57 +0100

    education

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ const Skill=require('./models/skills')
 const Hobbies=require('./models/hobbies')
 const Education=require('./models/education')
 const Project=require('./models/projects')
-const Experience=require('./models/experience')
+const Experience=require('./models/experience');
+const education = require('./models/education');
 
 
 
