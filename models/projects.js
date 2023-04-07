cfaf70cf153f2039b2273122ba883326e6f8aa86
bibinbabumj <bibinbabumj93@gmail.com>
2023-04-07 13:42:25 +0100

    Update projects.js

--- models/projects.js
+++ models/projects.js
@@ -1,40 +1,40 @@
-const mongosse=require("mongoose")
-const Schema= mongosse.Schema;
-
-const ProjectsSchema= new Schema({
-    user_id:{
-        type:String,
-        required:true
-    },
-
-    project_name:{
-        type:String,
-        required:true
-    }
-    ,
-
-    project_company_name:{
-        type:String,
-        required:true
-    }
-    ,
-
-    project_duration:{
-        type:String,
-        required:true
-    }
-    ,
-
-    project_details:{
-        type:String,
-        required:true
-    },
-
-    project_role:{
-        type:String,
-        required:true
-    }
-})
-
-module.exports=mongosse.model('Projects',ProjectsSchema)
-
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+let Projects = null;
+if (mongoose.models.Projects) {
+    Projects = mongoose.model('Projects');
+} else {
+    const ProjectsSchema= new Schema({
+        user_id:{
+            type:String,
+            required:true
+        },
+    
+        project_name:{
+            type:String,
+            required:true
+        },
+    
+        project_company_name:{
+            type:String,
+            required:true
+        },
+    
+        project_duration:{
+            type:String,
+            required:true
+        },
+    
+        project_details:{
+            type:String,
+            required:true
+        },
+    
+        project_role:{
+            type:String,
+            required:true
+        }
+    })
+    Projects = mongoose.model('Projects',ProjectsSchema)
+}
+module.exports = Projects;
