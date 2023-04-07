077c69057cec38a2c22e92016401063db070d07d
bibinbabumj <bibinbabumj93@gmail.com>
2023-04-07 11:29:41 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,14 +2,38 @@ const mongosse=require("mongoose")
 const Schema= mongosse.Schema;
 
 const UserSchema= new Schema({
-    title:{
+    _id:{
+        type:Int,
+        required:true
+    },
+    name:{
         type:String,
         required:true
     },
-    body:{
+
+    profile_url:{
         type:String,
         required:true
     },
+
+    location:{
+        type:String,
+        required:true
+    },
+
+    github_url:{
+        type:String,
+        required:true
+    },
+
+    gitlab_url:{
+        type:String,
+        required:true
+    },
+    linkedin_url:{
+        type:String,
+        required:true
+    }
 })
 
 module.exports=mongosse.model('User',UserSchema)
